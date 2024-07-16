ed1f15163f7bf94a2b8c67f6b74f2733b4bf2c71
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-07-15 22:11:48 -0300

    feat: add historico table

---
+++ prisma/migrations/20240716011142_create_historic_table/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Historic" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "companyId" TEXT NOT NULL,
+    "sellPrice" DOUBLE PRECISION NOT NULL,
+    "boughtPrice" DOUBLE PRECISION NOT NULL,
+    "lastQuantity" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "quantity" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Historic_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Historic" ADD CONSTRAINT "Historic_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Historic" ADD CONSTRAINT "Historic_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Historic" ADD CONSTRAINT "Historic_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
